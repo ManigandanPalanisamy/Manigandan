e104df7d0b3655cdfd5569efec60374a1f38466b
Manigandan P <manigandansanthosh1@gmail.com>
2016-08-26 06:52:38 -0700

    Update coins.c

--- coins.c
+++ coins.c
@@ -8,12 +8,7 @@ int number_of_coin(long int total)
 	   return number_of_coin(total%3)+total/3;
     if(total>=5)
 	 return number_of_coin(total%5)+total/5;
-
-
-	      
-	     
-	
-	 }
+ }
 int main(void) {
 	
 	long int total;

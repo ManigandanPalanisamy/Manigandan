dbb4afb571e58e4b79d0371d5fb0f5452fb21d40
Manigandan P <manigandansanthosh1@gmail.com>
2016-08-26 07:03:28 -0700

    Add files via upload

---
+++ large_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int  main()
+{
+int  a[50],b,c,i;
+clrscr();
+printf("Enter the number");
+scanf("%d",&c);
+for(i=0;i<a;i++)
+{
+scanf("%d",&a[i]);
+scanf("%d",&b);
+printf("%d",a[b]);
+}
+return 0;
+getch();
+}
